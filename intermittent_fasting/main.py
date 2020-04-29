a32527e818374bbbd2a5ee2e62b02dbf9c2b00ab
stephaniekrouthen <61312660+stephaniekrouthen@users.noreply.github.com>
2020-04-29 18:07:16 +0200

    form

--- intermittent_fasting/main.py
+++ intermittent_fasting/main.py
@@ -1,17 +1,27 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
-app = Flask("Hello World")
-
-
-@app.route('/hello')
-def hello_world():
-	return render_template('index.html', name="Stéphanie")
+app = Flask("Intermittent Fasting")
 
 
-@app.route("/test")
-def test():
-    return "success"
+@app.route('hello/<name>')
+def hello(name):
+	return render_template('index.html', name + "!")
+
+@app.route('/neues_rezept', methods=["GET", "POST"])
+def rezept():
+	if request.method == "POST":
+		rezept = request.from.to_dict()
+		rezept["rezepttitel"] = rezepttitel
+		rezept["zutaten"] = zutaten
+		rezept["zubereitung"] = zubereitung
+		rezept["kcal"] = kcal
+		rueckgabe_rezept = "Das Rezept" + rezepttitel + "wurde erfolgreich hinzugefügt!"
+		print(rezept)
+		return rueckgabe_rezept
+	return render_template('index/neues_rezept.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
