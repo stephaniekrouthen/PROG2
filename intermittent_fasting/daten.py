2f0539e9c7d0528a60f8af3d7e08c5ac7e40e4f9
stephaniekrouthen <61312660+stephaniekrouthen@users.noreply.github.com>
2020-05-16 00:29:19 +0200

    daten und json file

---
+++ intermittent_fasting/daten.py
@@ -0,0 +1,89 @@
+import json
+
+def speichern_namen(datei, value):
+    try:
+        with open(datei) as open_file:
+            datei_inhalt = json.load(open_file)
+    except FileNotFoundError:
+        datei_inhalt = {}
+
+    datei_inhalt["name"] = value
+
+def namen_speichern(name):
+    datei_name = "namen.json"
+    speichern_namen(datei_name, name)
+    return name
+
+def speichern_rezept(datei, titel, zutaten, zubereitung, kcal):
+    try:
+        with open(datei) as open_file:
+            datei_inhalt = json.load(open_file)
+    except FileNotFoundError:
+        datei_inhalt = {}
+
+    datei_inhalt[titel] = {"zutaten": zutaten, "zubereitung": zubereitung, "kcal": kcal}
+
+
+    # print(datei_inhalt)
+
+    with open(datei, "w") as open_file:
+        json.dump(datei_inhalt, open_file)
+
+
+def rezept_speichern(titel, zutaten, zubereitung, kcal):
+    datei_name = "rezepte.json"
+    speichern_rezept(datei_name, titel, zutaten, zubereitung, kcal)
+    return  titel, zutaten, zubereitung, kcal
+
+
+def rezepte_laden():
+    datei_name = "rezepte.json"
+
+    try:
+        with open(datei_name) as open_file:
+            datei_inhalt = json.load(open_file)
+    except FileNotFoundError:
+        datei_inhalt = {}
+
+    return datei_inhalt
+
+    with open(datei, "w") as open_file:
+        json.dump(datei_inhalt, open_file)
+
+
+def speichern_mahlzeiten(datei, key, value):
+    try:
+        with open(datei) as open_file:
+            datei_inhalt = json.load(open_file)
+    except FileNotFoundError:
+        datei_inhalt = {}
+
+    datei_inhalt[key] = value 
+
+
+    # print(datei_inhalt)
+
+    with open(datei, "w") as open_file:
+        json.dump(datei_inhalt, open_file)
+
+
+def mahlzeiten_speichern(rezept_titel, kalorien):
+    datei_name = "mahlzeiten.json"
+    speichern_mahlzeiten(datei_name, rezept_titel, kalorien)
+    return  rezept_titel, kalorien
+
+
+def mahlzeiten_laden():
+    datei_name = "mahlzeiten.json"
+
+    try:
+        with open(datei_name) as open_file:
+            datei_inhalt = json.load(open_file)
+    except FileNotFoundError:
+        datei_inhalt = {}
+
+    return datei_inhalt
+
+    with open(datei, "w") as open_file:
+        json.dump(datei_inhalt, open_file)
+
--- intermittent_fasting/main.py
+++ intermittent_fasting/main.py
@@ -1,27 +1,120 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+import daten
+import pandas as pd
 
 app = Flask("Intermittent Fasting")
 
+@app.route('/hello/')
+def hello():
+	return render_template('index.html', name="du")
 
-@app.route('hello/<name>')
-def hello(name):
-	return render_template('index.html', name + "!")
+@app.route('/hello/<name>')
+def hello_persoenlich(name=False):
+	if name:
+		return render_template('index.html', name=name)
+		name = daten.namen_speichern(name)
+	else:
+		return render_template('index.html')
 
-@app.route('/neues_rezept', methods=["GET", "POST"])
-def rezept():
+@app.route('/hello/neues_rezept.html', methods=["GET", "POST"])
+def rezept_speichern():
 	if request.method == "POST":
-		rezept = request.from.to_dict()
-		rezept["rezepttitel"] = rezepttitel
-		rezept["zutaten"] = zutaten
-		rezept["zubereitung"] = zubereitung
-		rezept["kcal"] = kcal
-		rueckgabe_rezept = "Das Rezept" + rezepttitel + "wurde erfolgreich hinzugefügt!"
-		print(rezept)
-		return rueckgabe_rezept
-	return render_template('index/neues_rezept.html')
+		titel = request.form['titel']
+		zutaten = request.form['zutaten']
+		zubereitung = request.form['zubereitung']
+		kcal = request.form['kcal']
+		bestätigung_rezept = "Das Rezept " + titel + " wurde erfolgreich zur Datenbank hinzugefügt!"
+		titel, zutaten, zubereitung, kcal = daten.rezept_speichern(titel, zutaten, zubereitung, kcal)
+		return render_template('neues_rezept.html', bestätigung_rezept=bestätigung_rezept)
 
+	return render_template('neues_rezept.html')
+
+
+
+@app.route('/hello/kalorienbilanz.html',  methods=["GET", "POST"])
+def mahlzeiten_speichern():
+	if request.method == "POST":
+		rezept_titel = request.form['rezept_titel']
+		data = daten.rezepte_laden()
+		kalorien = data[rezept_titel]["kcal"]
+		bestätigung_mahlzeit = "Die Mahlzeit " + rezept_titel + " wurde erfolgreich zu deinem Fastentag hinzugefügt!"
+		rezept_titel, kalorien = daten.mahlzeiten_speichern(rezept_titel, kalorien)
+		mahlzeiten = daten.mahlzeiten_laden()
+
+		kalorienbilanz = 0
+		for kcal in mahlzeiten.values():
+			kcal = int(kcal)
+			kalorienbilanz += kcal
+		return render_template('kalorienbilanz.html', bestätigung_mahlzeit=bestätigung_mahlzeit, kalorienbilanz=kalorienbilanz, )
+
+	return render_template('kalorienbilanz.html')
+
+
+
+@app.route('/hello/rezepte.html')
+def uebersicht():
+    rezepte = daten.rezepte_laden()
+
+    rezepte_liste = ""
+    for key, value in rezepte.items():
+    	titel = key + " - "
+    	rezepte_liste += titel
+    	for key, value in rezepte[key].items():
+        	zeile = str(key) + ": " + str(value) + "\n"
+        	rezepte_liste += zeile
+
+
+    return render_template('rezepte.html', rezepte_liste=rezepte_liste)
+
+
+"""
+@app.route('/pandas/')
+def data():
+	data = daten.rezepte_laden()
+	data_df = pd.DataFrame(data)
+	return print(data)
+
+
+
+@app.route('/hello/neues_rezept.html', methods=["GET", "POST"])
+def rezept_hinzufügen():
+	if request.method == "POST":
+		ende = "Die Rezept-Eingabe wurde beendet!"
+		rezepte = {}
+		rezept_titel = request.form['titel']
+		rezept_zutaten = request.form['zutaten']
+		rezept_zubereitung = request.form['zubereitung']
+		rezept_kcal = request.form['kcal']
+		weiteres_rezept = request.form['mehr']
+		while weiteres_rezept != "n":
+			for eintrag in request.form:
+				rezepte[rezept_titel] = {
+					"zutaten": rezept_zutaten,
+					"zubereitung": rezept_zubereitung,
+					"kcal": rezept_kcal,
+				}
+				rueckgabe_rezept = "Das Rezept " + rezept_titel + " wurde zur Datenbank hinzugefügt!"
+				return render_template('neues_rezept.html', bestätigung_rezept=rueckgabe_rezept, übersicht=rezepte)
+		return render_template('neues_rezept.html', ende=ende)
+	return render_template('neues_rezept.html')
+
+"""
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
+
+"""
+	return render_template('neues_rezept.html', bestätigung_rezept=rueckgabe_rezept)
+	return render_template('neues_rezept.html')
+
+
+Für mehrere Rezepte, Schleifen?
+Das Dict rezept muss irgendwo abgespeichert werden, damit es dann im mahlzeit_hinzufügen.html abgerufen werden kann 
+und damit mehrere Dicts erstellt werden können.
+rekursive funktion
+"""
+
+
+
