34dcb97d99375b1cf1a2681d61cea90192214b16
stephaniekrouthen <61312660+stephaniekrouthen@users.noreply.github.com>
2020-06-06 17:31:30 +0200

    kommentare und letzte Anpassungen

--- intermittent_fasting/daten.py
+++ intermittent_fasting/daten.py
@@ -1,7 +1,7 @@
 import json
 
 
-def speichern_rezept(datei, titel, zutaten, zubereitung, kcal):
+def speichern_rezept(datei, titel, zutaten, zubereitung, kcal): #daten in Klammern wurde von der Funktion rezept_speichern übergeben
     try:
         with open(datei) as open_file:
             datei_inhalt = json.load(open_file)
@@ -10,20 +10,17 @@ def speichern_rezept(datei, titel, zutaten, zubereitung, kcal):
 
     datei_inhalt[titel] = {"zutaten": zutaten, "zubereitung": zubereitung, "kcal": kcal} #Aufbau des Json-Files rezepte.json
 
-
-    # print(datei_inhalt)
-
     with open(datei, "w") as open_file:
         json.dump(datei_inhalt, open_file)
 
 
-def rezept_speichern(titel, zutaten, zubereitung, kcal):
+def rezept_speichern(titel, zutaten, zubereitung, kcal): #die Daten in der Klammer wurden von main.py an diese Funktion übergeben
     datei_name = "rezepte.json"
     speichern_rezept(datei_name, titel, zutaten, zubereitung, kcal) #daten an funktion speichern_rezepte übergeben
     return  titel, zutaten, zubereitung, kcal
 
 
-def rezepte_laden():
+def rezepte_laden(): #den Inhalt des rezepte.json aufrufen können
     datei_name = "rezepte.json"
 
     try:
@@ -38,32 +35,32 @@ def rezepte_laden():
         json.dump(datei_inhalt, open_file)
 
 
-def speichern_mahlzeiten(datei, key, value):
+def speichern_mahlzeiten(datei, key, value): #daten in klammer von mahlzeiten_speichern übergeben
     try:
         with open(datei) as open_file:
             datei_inhalt = json.load(open_file)
     except FileNotFoundError:
         datei_inhalt = {}
 
-    datei_inhalt[key] = value 
+    datei_inhalt[key] = value #key ist Rzepttitel und value ist Azahl kcal
 
     with open(datei, "w") as open_file:
         json.dump(datei_inhalt, open_file)
 
 
-def mahlzeiten_speichern(tag, username, rezept_titel, kalorien):
+def mahlzeiten_speichern(tag, username, rezept_titel, kalorien): #Daten in der Klammer von main.py übergeben
     datei_name = "mahlzeiten-" + username + "-" + str(tag) + ".json" #für jeden Nutzer und jeden Tag ein neues json-File erstellen
     try:
-        with open(datei_name) as open_file:
+        with open(datei_name) as open_file: #überprüfen ob es schon ein Json-File für diesen Nutzer und diesen Tag gibt
             datei_inhalt = json.load(open_file)
-    except FileNotFoundError:
+    except FileNotFoundError: #ansonsten ein neues File für Nutzer un Tag erstellen
         with open(datei_name, "w") as open_file:
             open_file.write("{}")
-    speichern_mahlzeiten(datei_name, rezept_titel, kalorien)
+    speichern_mahlzeiten(datei_name, rezept_titel, kalorien) #daten in Klammern an speichern_mahlzeiten übergeben
     return  rezept_titel, kalorien
 
 
-def mahlzeiten_laden(username, tag):
+def mahlzeiten_laden(username, tag): #daten in klammer von main.py erhalten
     datei_name = "mahlzeiten-" + username + "-" + str(tag) + ".json"
     try:
         with open(datei_name) as open_file:
@@ -71,7 +68,7 @@ def mahlzeiten_laden(username, tag):
     except FileNotFoundError:
         datei_inhalt = {}
 
-    return datei_inhalt
+    return datei_inhalt #den Inhalt des rezepte.json aufrufen können
 
     with open(datei, "w") as open_file:
         json.dump(datei_inhalt, open_file)
--- intermittent_fasting/main.py
+++ intermittent_fasting/main.py
@@ -1,34 +1,14 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-import daten
-import pandas as pd
-import plotly.graph_objects as go
+import daten #daten.py importieren
+import plotly.graph_objects as go 
 from plotly.offline import plot
 from datetime import date
 
 
 app = Flask("Intermittent Fasting")
 
-"""
-Nicht verwendete oder eingebaute Grafik zur Darstellung der Kalorienbilanz:
-def plotly(): 
-	mahlzeiten = daten.mahlzeiten_laden()
-	kalorien_bilanz = 0
-	for kcal in mahlzeiten.values():
-		kcal = int(kcal)
-		kalorien_bilanz += kcal
-	verbraucht = (100/500)*kalorien_bilanz
-	verfuegbar = 100 - verbraucht
-	colors = ['mediumturquoise', 'lightgreen']
-	labels = ['verbraucht', 'verfügbar']
-	values = [verbraucht, verfuegbar]
-
-	fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole= 0.9)])
-	fig.update_traces(hoverinfo='label+percent', textinfo='value', textfont_size=20, marker=dict(colors=colors, line=dict(color='#000000', width=2)))
-	div = plot(fig, output_type="div")
-	return div
-"""
 
 @app.route('/')
 def index():
@@ -39,7 +19,7 @@ def rezept_speichern():
 	if request.method == "POST":
 		titel = request.form['titel']
 		wort_liste = titel.split()
-		wort_anzahl = len(wort_liste)
+		wort_anzahl = len(wort_liste) #Anzahl Zeichen des Rezepttitels ermitteln
 		zutaten = request.form['zutaten']
 		zubereitung = request.form['zubereitung']
 		kcal = request.form['kcal']
@@ -50,7 +30,6 @@ def rezept_speichern():
 			bestätigung_rezept = "Das Rezept " + titel + " wurde erfolgreich zur Datenbank hinzugefügt!"
 			titel, zutaten, zubereitung, kcal = daten.rezept_speichern(titel, zutaten, zubereitung, kcal) #eingaben vom Formular in daten.json übergeben und abspeichern
 			return render_template('neues_rezept.html', bestätigung_rezept=bestätigung_rezept)
-
 	return render_template('neues_rezept.html')
 
 
@@ -66,19 +45,34 @@ def mahlzeiten_speichern():
 			bestätigung_mahlzeit = "Die Mahlzeit " + rezept_titel + " wurde erfolgreich zu deinem Fastentag hinzugefügt!"
 			rezept_titel, kalorien = daten.mahlzeiten_speichern(tag, username, rezept_titel, kalorien) #Informationen an daten.json übergeben, um die Mahlzeit abzuspeichern
 			mahlzeiten = daten.mahlzeiten_laden(username, tag) #auch diese funktion benötigt username und tag damit der korrekte Dateiname aufgerufen werden kann
+			
+			#Verbrauchte Kcal-Anzahl berechnen
 			kcal_verbraucht = 0
 			for kcal in mahlzeiten.values(): #alle kcal in mahlzeiten.json der entsprechenden Person sollen zusammengerechnet werden
 				kcal = int(kcal)
 				kcal_verbraucht += kcal
+			kcal_verfuegbar = 500-kcal_verbraucht #ermitteln, wieviele kcal von denn 500 noch verfügbar sind
+
+			#Rückmeldung der Applikation wieviele kcal verfügbar sowie verbraucht sind
 			kcal_verbraucht_str = username + ", du hast heute bereits " + str(kcal_verbraucht) + " gegessen."
-			kcal_verfuegbar = 500-kcal_verbraucht
 			kcal_verfuegbar_str = "Das heisst, du hast noch " + str(kcal_verfuegbar) + " Kalorien verfügbar heute."
+
+			#Plotly-Grafik:
+			colors = ['mediumturquoise', 'lightgreen']
+			labels = ['verbraucht', 'verfügbar'] #Beschriftung der Plotly-Grafik
+			values = [kcal_verbraucht, kcal_verfuegbar] #Werte der Plotly-Grafik
+			fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole= 0.9)]) #Angaben für die Plotly-Grafik
+			fig.update_traces(hoverinfo='label', textinfo='value', textfont_size=20, marker=dict(colors=colors, line=dict(color='#000000', width=2))) #Formatierung der Grafik
+			grafik = plot(fig, output_type="div") #Plotly in der Variable "grafik" abspeicher, um sie dann ins html übergeben zu können
+
+			#ermitteln, ob max kcal-Anzahl von 500 bereits überschritten wurde
 			if kcal_verfuegbar <= 0:
-				kcal_ueberschritten = "Das heist, du hast heute deinen Kalorienbedarf um " + str(kcal_verfuegbar) + " überschritten!"
-				kcal_verfuegbar = kcal_ueberschritten
-				return render_template('kalorienbilanz.html', kcal_ueberschritten=kcal_ueberschritten, kcal_verfuegbar=False, bestätigung_mahlzeit=bestätigung_mahlzeit, kcal_verbraucht_str=kcal_verbraucht_str)
-			return render_template('kalorienbilanz.html', bestätigung_mahlzeit=bestätigung_mahlzeit, kcal_verbraucht_str=kcal_verbraucht_str, kcal_verfuegbar_str=kcal_verfuegbar_str)
-		else:
+				kcal_ueberschritten = "Das heisst, du hast heute deinen Kalorienbedarf um " + str(-1*kcal_verfuegbar) + " überschritten!"
+				kcal_verfuegbar = kcal_ueberschritten #kcal verfügbar wurde mit kcal überschritten überschrieben
+				return render_template('kalorienbilanz.html', kcal_ueberschritten=kcal_ueberschritten, kcal_verfuegbar=False, bestätigung_mahlzeit=bestätigung_mahlzeit, kcal_verbraucht_str=kcal_verbraucht_str, viz_div=grafik, username=username)
+			
+			return render_template('kalorienbilanz.html', bestätigung_mahlzeit=bestätigung_mahlzeit, kcal_verbraucht_str=kcal_verbraucht_str, kcal_verfuegbar_str=kcal_verfuegbar_str, viz_div=grafik, username=username)
+		else: #wenn das Rezept nicht gefunden werden kann, kommt diese Fehlermeldung
 			fehler_meldung = "Das gewählte Rezept ist nicht in der Datenbank vorhanden. Bitte prüfe die Schreibweise (Gross- und Kleinschreibung beachten!).\
 			Am besten kopierst du den Rezeptnamen direkt von der Übersicht! Falls das Rezept noch nicht vorhanden ist, füge es zur Datenbank hinzu."
 			return render_template('kalorienbilanz.html', fehler_meldung=fehler_meldung)
@@ -87,31 +81,22 @@ def mahlzeiten_speichern():
 
 @app.route('/rezepte.html')
 def uebersicht():
-    rezepte = daten.rezepte_laden()
+    rezepte = daten.rezepte_laden() #Inhalt von rezepte.json als ditc in variable "rezepte" speichern
     rezepte_liste = ""
     for key, value in rezepte.items():
     	zeichen_laenge = len(key)
     	binde_striche = "_" * (130-zeichen_laenge) #damit der Rest der Zeile mit Bindestrichen aufgefüllt wird und der weitere Inhalt auf eine neue Zeile kommt
     	titel = key + binde_striche
-    	zutaten = rezepte[key]["zutaten"]
+    	zutaten = rezepte[key]["zutaten"] #die zugehörigen Zutaten des entsprechenden Rezepttitels aufrufen
     	zubereitung = rezepte[key]["zubereitung"]
     	kcal = rezepte[key]["kcal"]
-    	rezepte_liste += titel + " Zutaten: " + zutaten + " // Zubereitung: " + zubereitung + " // Anzahl Kalorien: " + kcal + " // " #Zeilenumbruch konnte mit "<br>" oder "\n" nicht erreicht werden
-    	
+    	rezepte_liste += titel + " Zutaten: " + zutaten + " // Zubereitung: " + zubereitung + " // Anzahl Kalorien: " + kcal + " // " #Zeilenumbruch konnte mit "<br>" oder "\n" nicht erreicht werden 	
     return render_template('rezepte.html', rezepte_liste=rezepte_liste)
 
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
 
-"""
-
-
-
-name abspeichern, dass immer Hallo name kommt -> json
-Grafik -> im HTML integrieren
-
-"""
 
 
 
